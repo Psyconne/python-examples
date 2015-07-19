58f3a203d76254250aae9b2595a51a39d001d56f
psyconne <iimen.elidrissi@gmail.com>
2015-07-20 01:09:15 +0200

    prime numbers

--- ex37.py
+++ ex37.py
@@ -1,7 +1,8 @@
+#
 def f(x):
 	return x**2
 print f(4)
-
+#incrementor
 def incrementor (n): 
 	return lambda x: x+n
 
@@ -10,5 +11,14 @@ print g(34)
 print incrementor(2)(10)
 
 foo = [1, 22, 9, 12, 32]
+#filter
 print filter (lambda x: x%3 == 0, foo)
+#map
 print map(lambda x: x*2+10, foo)
+#sum
+print reduce(lambda x, y: x+y, foo)
+#prime numbers
+nums = [1, 13, 22, 24, 52, 12, 7, 8, 11]
+for i in range (2, 7):
+	nums = filter (lambda x: x==i or x%i, nums)
+print nums
