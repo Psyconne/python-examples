912e80eb4097eaf4906c5aee17d1e5ed37207457
psyconne <iimen.elidrissi@gmail.com>
2015-07-20 04:56:28 +0200

    lists

---
+++ ex38.py
@@ -0,0 +1,25 @@
+ten_things = "Apple Orange Banana Carrot"
+print "\nWait there are not 10things. Okai let's fix that"
+stuff = ten_things.split(' ')
+more_stuff = ["one", "two", "school", "books", "sky", "home"]
+
+while len(stuff) != 10:
+	next_stuff = more_stuff.pop()
+	print "adding: ", next_stuff	
+	stuff.append(next_stuff) 
+	print "There are %d items" %len(stuff)
+
+#['Apple', 'Orange', 'Banana', 'Carrot', 'home', 'sky', 'books', 'school', 'two', 'one']
+print "That's it: \n", stuff
+
+print "\n Okai, let's do some things with stuff"
+
+print stuff[-1]
+print stuff[1]
+
+#one
+print stuff.pop() # this is how python do his job ===> pop(stuff) 
+#Apple Orange Banana Carrot home sky books school two
+print ' '.join(stuff)
+#Banana#Carrot#home
+print '#'.join(stuff[2:5])
